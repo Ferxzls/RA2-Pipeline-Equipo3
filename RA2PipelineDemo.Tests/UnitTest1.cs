01a43a9d16e4cc449f33d885b1046b344b059088
Ferxzls <rpliknows5@gmail.com>
2026-02-24 22:27:59 -0600

    Add files via upload

---
+++ RA2PipelineDemo.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using RA2PipelineDemo;
+
+namespace RA2PipelineDemo.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Sum_ReturnsCorrectValue()
+        {
+            var calc = new Calculator();
+            var result = calc.Sum(2, 3);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ RA2PipelineDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RA2PipelineDemo
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto listo.");
+        }
+    }
+}
